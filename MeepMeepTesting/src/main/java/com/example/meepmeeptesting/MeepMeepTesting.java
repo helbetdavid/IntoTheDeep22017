b90c6f110c5cdfc170162cf39a988f42e8452c4a
swif <horiagavril@gmail.com>
2024-12-20 11:45:53 +0200

    Parcare sucara

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,7 +14,7 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16.14)
                 .build();
-        int x = 1;
+        int x = 2;
         if (x == 1)
         {
             myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(7.61, 62.8, Math.toRadians(-90)))
@@ -28,11 +28,6 @@ public class MeepMeepTesting {
                     .strafeToLinearHeading(new Vector2d(55, 50), Math.toRadians(45))
                     .strafeToLinearHeading(new Vector2d(35, 8),Math.toRadians(180))
                     .strafeTo(new Vector2d(28, 8))
-
-
-
-
-
                     .build());
 
             meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
@@ -43,18 +38,76 @@ public class MeepMeepTesting {
         }
         else if(x==2)
         {
-            myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(7.61, 62.8, Math.toRadians(-90)))
-                    .strafeTo(new Vector2d(4.8, 34.5 ))
-                    .strafeTo(new Vector2d(4.8,33))
-                    .strafeTo(new Vector2d(50,46))
-                    .strafeToLinearHeading(new Vector2d(54,50),Math.toRadians(44.78))
-                    .strafeToLinearHeading(new Vector2d(60.5,46),Math.toRadians(-90))
-                    .strafeToLinearHeading(new Vector2d(54,50),Math.toRadians(44.78))
-                    .strafeToLinearHeading(new Vector2d(55,40),Math.toRadians(-45))
-                            .waitSeconds(100)
-                    .strafeToLinearHeading(new Vector2d(54,50),Math.toRadians(44.78))
-                    .strafeToLinearHeading(new Vector2d(31, 8),Math.toRadians(0))
-                    .strafeTo(new Vector2d(28, 8))
+            myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-14, 62.8, Math.toRadians(-90)))
+//                    .strafeToLinearHeading(new Vector2d(-3.8, 33.3), -89.54)
+//                    .strafeTo(new Vector2d(-31, 43))
+//                    .setTangent(60)
+//                    .splineToSplineHeading(new Pose2d(-46, 13, 89.54), Math.PI)
+//                            .strafeTo(new Vector2d(-45, 55))
+//                            .setTangent(-1)
+//                            .splineToLinearHeading(new Pose2d(-58, 13, 89.54), Math.PI)
+//                            .strafeTo(new Vector2d(-58, 50))
+//                            .setTangent(-1)
+//                            .splineToLinearHeading(new Pose2d(-60, 13, 89.54), Math.PI)
+//                            .strafeTo(new Vector2d(-60, 50))
+//                            .strafeTo(new Vector2d(-37,50))
+//                            .strafeTo(new Vector2d(-37,55))
+
+
+                    .strafeToLinearHeading(new Vector2d(-3.8, 33.3), -89.54)
+                    .strafeTo(new Vector2d(-31, 43))
+                    .setTangent(60)
+                    .splineToSplineHeading(new Pose2d(-46, 13, 89.54), Math.PI)
+                    .strafeTo(new Vector2d(-45, 55))
+                    .setTangent(-1)
+                    .splineToLinearHeading(new Pose2d(-58, 13, 89.54), Math.PI)
+                    .strafeTo(new Vector2d(-58, 50))
+                    .setTangent(-1)
+                    .splineToLinearHeading(new Pose2d(-61, 13, Math.toRadians(180)), Math.PI)
+                    .strafeTo(new Vector2d(-61, 50))
+                    .strafeTo(new Vector2d(-23.33, 57.5))
+                    .strafeToLinearHeading(new Vector2d(-3.8, 33.3), Math.toRadians(-90))
+                    .strafeToLinearHeading(new Vector2d(-23.33, 57.5), Math.toRadians(180))
+                    .strafeToLinearHeading(new Vector2d(-3.8, 33.3), Math.toRadians(-90))
+                    .strafeToLinearHeading(new Vector2d(-23.33, 57.5), Math.toRadians(180))
+                    .strafeToLinearHeading(new Vector2d(-3.8, 33.3), Math.toRadians(-90))
+                    .strafeTo(new Vector2d(-49, 60))
+//                    .strafeTo(new Vector2d(-37, 50))
+//                    .strafeTo(new Vector2d(-37, 55))
+
+
+
+
+
+
+
+//                    .build()
+//                    .strafeTo(new Vector2d(-46, 13))
+//                    .strafeTo(new Vector2d(-46, 54))
+//                    .waitSeconds(3)
+//                    //.setTangent(1)
+//                    //.splineToSplineHeading(new Pose2d(-3.8, 33.3,-90), Math.PI/ 2)
+//                    .strafeToLinearHeading(new Vector2d(-3.8, 33.3), -89.54)
+//                    .setTangent(1)
+//                    .splineToSplineHeading(new Pose2d(-48, 54, 89.54), Math.PI / 2)
+//                    .strafeToLinearHeading(new Vector2d(-3.8, 33.3), -89.54)
+//                    .strafeTo(new Vector2d(-54, 59))
+
+//              .turn(Math.toRadians(90))
+//              .lineToY(30)
+//              .turn(Math.toRadians(90))
+//              .lineToX(0)
+//              .turn(Math.toRadians(90))
+                    .waitSeconds(999999999)
+                            .turn(Math.toRadians(90))
+
+
+
+
+
+
+
+
                     .build());
 
             meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
