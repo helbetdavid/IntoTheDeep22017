fc3366f53d2113acfd08ad4413be6d611f6ed48f
ardey <albuandreicezar@gmail.com>
2025-02-06 16:26:27 +0200

    bla bla bla

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpMariusAlbastru.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpMariusAlbastru.java
@@ -172,8 +172,8 @@ public class TeleOpMariusAlbastru extends LinearOpMode {
                     xCam = limeLight.getTargetTx();
                     yCam = limeLight.getTargetTy();
 //
-                    xReal = -(Math.tan(Math.toRadians(limeLight.getTargetTx())) * (11.2/384)*lift.getPosition());
-                    yReal = -(Math.tan(Math.toRadians(limeLight.getTargetTy())) * (11.2/384)*lift.getPosition());
+                    xReal = - (Math.tan(Math.toRadians(limeLight.getTargetTx())) * (((double) 11.2 /384) * lift.getPosition())+7.5);
+                    yReal = - (Math.tan(Math.toRadians(limeLight.getTargetTy())) * (((double) 12.2/384) * lift.getPosition())+7.5);
 
                     ticks = xReal * 341.3;
                     ticksext = (yReal - 2.8) * 11.76;
